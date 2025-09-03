33b31f1074d7f4c48636c13606f73cbd5007cac3
RUYvo <engs-ruyjunior@camporeal.edu.br>
2025-09-03 16:52:12 -0300

    feat: beginning of the development of a registration and login system with auth using the api, still incomplete

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3001', // URL do frontend
+    credentials: true, // se precisar enviar cookies ou autenticação
+  });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
